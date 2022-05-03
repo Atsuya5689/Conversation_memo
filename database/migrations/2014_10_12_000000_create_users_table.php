cd2b697991d8b44d0324aa5b648f607de1fc533c
Atsuy5689 <atsu8.uchida44@gmail.com>
2022-05-03 16:05:06 +0900

    エラーの修正

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -21,9 +21,10 @@ class CreateUsersTable extends Migration
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
-            $table->integer('company_id');
-            $table->string('gender');
-            $table->integer('age');
+            $table->integer('company_id')->nullable();
+            $table->string('gender')->nullable();
+            $table->integer('age')->nullable();
+            $table->softDeletes();
         });
     }
 
